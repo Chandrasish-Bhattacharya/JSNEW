e79a1fb662e5e4e67a3896e7c7efdbad1c53cacd
Chandarasish Bhattacharya <chandrasish3456@gmail.com>
2025-07-19 23:33:04 +0530

    Initial commit

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 11500122077
+let accountName = "Chandrasish Bhattacharya"
+var accountPassword = 97225230
+accountCity = "Kolkata"
+
+// normal way to print a variables 
+console.log(accountId)
+// other way to print a value in table format 
+console.table([accountId,accountName,accountPassword,accountCity])
---
+++ 01_Basic/02_Datatypes.js
@@ -0,0 +1,2 @@
+/* "use strict" //treats all Js code in as new version*/
+// alert(3+3) we are using nodejs , not browser 
