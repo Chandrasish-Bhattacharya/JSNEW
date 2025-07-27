ef5563dd96985a12c26a019254ba3963f69dbdc6
Chandarasish Bhattacharya <chandrasish3456@gmail.com>
2025-07-27 19:57:18 +0530

    Add conversion operators example and remove README file

---
+++ 01_Basic/03_ConversionOperators.js
@@ -0,0 +1,9 @@
+let score = "100"; // this is a Number type
+console.log(typeof score); // "number"
+let scoreString = Number(score); // converting number to string
+console.log(typeof scoreString); // "string"
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1
+// false => 0
