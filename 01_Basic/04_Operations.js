0063507205dcb67c50d890f0a8cd561add2abca8
Chandarasish Bhattacharya <chandrasish3456@gmail.com>
2025-07-27 20:43:05 +0530

    This is a new file

---
+++ 01_Basic/04_Operations.js
@@ -0,0 +1,4 @@
+// lets start with making a value negative
+let value = 7
+let negvalue = -value // this will make the value negative
+console.log(negvalue)
---
+++ 01_Basic/04_OperationsFun.js
@@ -0,0 +1,26 @@
+// lets start with making a value negative
+let value = 7
+let negvalue = -value // this will make the value negative
+// console.log(negvalue)
+
+// basic arithmetic operations
+// console.log(2-2) // subtraction operator
+// console.log(2+2) // addition operator
+// console.log(2*2) // multiplication operator
+// console.log(2**3) // exponentiation operator
+// console.log(2/2)   // division operator
+// console.log(2%2) // modulus operator
+
+// string concatenation
+let firstName = "Chandrasish"
+let lastName = "Bhattacharya"
+// console.log(firstName + " " + lastName); // concatenation operator
+
+// console.log("1" + 2) // string concatenation with number
+// console.log(1 + "2") // number concatenation with string
+// console.log("1" + "2") // string concatenation with string
+// console.log(1 + 2 + "3") // number addition followed by string concatenation
+
+console.log(+true)
+console.log(+false) // converting boolean to number
+console.log(+"")
